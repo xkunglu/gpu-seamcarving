761cf7d99a5534e7985b6df05f3014dc363f9dab
Unknown <xkunglu@gmail.com>
2018-04-12 12:48:25 -0700

    fixed missing import, added examples of failure cases

--- sc_utils.py
+++ sc_utils.py
@@ -4,6 +4,8 @@
 import os
 import subprocess as sp
 import json
+from shutil import copyfile
+import time
 
 def ffgetsize(file_in):
     """fast method to get image size in python"""
@@ -82,3 +84,37 @@ def remove_seams(file_in, file_out, n_seams, gpu=False):
     proc = sp.Popen(_fcmd, stdin=sp.PIPE, stderr=sp.PIPE, stdout=sp.PIPE)
     out, err = proc.communicate()
     return out, err
+
+def to_square(file_in, gpu=False):
+    """seam carve rectangular images to square"""
+    _rotate = False
+    _in_images = 'images'
+    _out_images = 'outputs'
+
+    _temp_fname = set_file(_in_images, file_in, '.bmp')
+    _out_fname = set_file(_out_images, file_in, '.bmp')
+
+    _file = get_file(_in_images, file_in)
+    _imsize = ffgetsize(_file)
+    _fmt = ffgetformat(_file)
+
+    #image is already square, do nothing
+    if _imsize[0] == _imsize[1]:
+        copyfile(file_in, os.path.join(_out_images, os.path.basename(file_in)))
+
+    # convert to bgra .bmp, rotate if necessary, rescale to 1024 maxwidth
+    ffconvert(_file, _temp_fname, _imsize)
+
+    n_seams = abs(_imsize[0]- _imsize[1])
+    maxsize = max(_imsize[0], _imsize[1])
+    if maxsize > 1024:
+        n_seams = int(n_seams*1024/maxsize)
+
+    remove_seams(_temp_fname, _out_fname, n_seams=n_seams, gpu=gpu)
+
+    if _imsize[1] > _imsize[0]:
+        ffrotate(_out_fname, _out_fname)
+    
+    time.sleep(0.3)
+
+    return _file, _out_fname
